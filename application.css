2372296329360156d2eb39caab8a01e04811c32c
akshay koti <62739437+pitstop260@users.noreply.github.com>
2021-12-24 13:11:45 +0530

    Create application.css

---
+++ application.css
@@ -0,0 +1,132 @@
+:root {
+  /* Primary */
+
+  --Custom1: hsl(210, 91%, 22%);
+  --Custom2: hsl(84, 50%, 92%);
+  --Custom3: #17bebb;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Chau Philomene One", sans-serif;
+  color: var(--Custom1);
+}
+
+body {
+  display: grid;
+  height: auto;
+  grid-template-rows: 5% 87% 8%;
+  grid-template-areas:
+    "header"
+    "main"
+    "footer";
+}
+
+header {
+  background-color: var(--SoftBlue);
+  color: var(--Custom1);
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  grid-area: "header";
+}
+
+header p {
+  font-size: 30px;
+  padding: 12px;
+}
+
+nav img {
+  padding: 10px;
+  width: 60px;
+}
+
+main {
+  grid-area: "main";
+  text-align: center;
+}
+
+h1 {
+  color: var(--Custom1);
+  margin-top: 10px;
+}
+
+#skip p {
+  text-align: center;
+  font-size: 20px;
+}
+
+#skip div {
+  display: flex;
+  justify-content: center;
+}
+
+#skip img {
+  width: 60px;
+}
+
+#card__heading {
+  margin-top: 10px;
+}
+
+.cards {
+  height: 13em;
+  width: 10em;
+  border-radius: 10px;
+  margin: 20px auto 20px auto;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  background: #d1913c; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #ffd194,
+    #d1913c
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #ffd194,
+    #d1913c
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  cursor: pointer;
+}
+
+.cards:hover {
+  transition: 0.1s;
+  box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px,
+    rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
+    rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+}
+
+.adjust {
+  height: 90%;
+}
+
+.extra-info {
+  text-align: left;
+  height: 30%;
+  width: 100%;
+  padding-left: 5%;
+  padding-top: 10px;
+  margin-top: 81%;
+}
+
+footer {
+  grid-area: "footer";
+  width: 100%;
+  text-align: center;
+}
+
+@media only screen and (min-width: 600px) {
+  #full__window {
+    /* border: 1px solid black; */
+    margin-top: 40px;
+    display: flex;
+  }
+  .cards {
+    /* border: 1px solid black; */
+    width: 12em;
+    height: 15em;
+  }
+}
